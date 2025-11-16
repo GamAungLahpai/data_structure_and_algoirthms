002f1b2f5927568b3b9678f910eb2c8c8947f54e
Twe He Gam Aung <Gam.TweHe@metropolia.fi>
2025-11-16 16:52:31 +0200

    Demo of Linear Seach and Binary Search

--- src/linear_binary_search/com/Demo.java
+++ src/linear_binary_search/com/Demo.java
@@ -1,11 +1,29 @@
 package linear_binary_search.com;
 
+/**
+ * This demo works with a sorted array.
+ *
+ * Linear Search:
+ * - Checks elements one by one until the target is found.
+ * - As the array grows larger, it requires more steps.
+ * - Time Complexity: O(n)
+ * - Space Complexity: O(1)
+ *
+ * Binary Search:
+ * - Works only on sorted arrays.
+ * - Repeatedly divides the search range in half.
+ * - When the array size doubles, it needs only one additional step.
+ * - Time Complexity: O(log n)
+ * - Space Complexity: O(1)
+ */
+
 public class Demo {
     public static void main(String[] args) {
-        int num[] = {5, 7, 9, 11, 13};
-        int target = 15;
+        int num[] = {1,2,3,4,5, 7, 9, 11, 13};
+        int target = 13;
 
         int result = linearSearch(num, target);
+        int result1 = binarySearch(num, target);
 
         if (result != -1) {
             System.out.println("Element Found at index " + result);
@@ -13,17 +31,50 @@ public class Demo {
             System.out.println("Element not found");
         }
 
+        if (result1 != -1) {
+            System.out.println("Element Found at index " + result1);
+        } else {
+            System.out.println("Element not found");
+        }
+
+
     }
 
     public static int linearSearch(int[] num, int target) {
-        for(int i= 0; i< num.length;i++){
-            if(num[i] == target){
+        int steps = 0;
+        for (int i = 0; i < num.length; i++) {
+            steps++;
+            if (num[i] == target) {
+                System.out.println("Steps taken by linear: " + steps);
                 return i;
+            }
+        }
+        System.out.println("Steps taken by linear: " + steps);
+        return -1;
+    }
+
+
+    public static int binarySearch(int[] num, int target) {
+        int steps = 0;
+        int left = 0;
+        int right = num.length - 1;
+        while (left <= right) {
+            steps++;
+            int mid = (left + right) / 2;
+
+            if (num[mid] == target) {
+                System.out.println("Steps taken by binary: " + steps);
+                return mid;
+            } else if (num[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
 
         }
-
+        System.out.println("Steps taken by binary: " + steps);
         return -1;
+
     }
 
 
